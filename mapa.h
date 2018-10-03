9ae80585b4770d6a5ffdb1397b5354a216731250
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-10-03 13:35:34 -0300

    Finalização do curso de C

--- mapa.h
+++ mapa.h
@@ -1,35 +1,38 @@
-#define HEROI '@'
-#define FANTASMA 'F'
-#define VAZIO '.'
-#define PAREDE_VERTICAL '|'
-#define PAREDE_HORIZONTAL '-'
-#define PILULA 'P'
+#ifndef _MAPA_H_
+    #define _MAPA_H_
 
-struct mapa {
-    char** matriz;
-    int linhas;
-    int colunas;
-};
+    #define HEROI '@'
+    #define FANTASMA 'F'
+    #define VAZIO '.'
+    #define PAREDE_VERTICAL '|'
+    #define PAREDE_HORIZONTAL '-'
+    #define PILULA 'P'
 
-typedef struct mapa MAPA;
+    struct mapa {
+        char** matriz;
+        int linhas;
+        int colunas;
+    };
 
-struct posicao {
-	int x;
-	int y;
-};
+    typedef struct mapa MAPA;
 
-typedef struct posicao POSICAO;
+    struct posicao {
+        int x;
+        int y;
+    };
 
-void liberamapa(MAPA* m);
-void lemapa(MAPA* m);
-void alocamapa(MAPA* m);
-void imprimemapa(MAPA* m);
-int ehvalida(MAPA* m, int x, int y);
-int ehvazia(MAPA* m, int x, int y);
-void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
-void copiamapa(MAPA* destino, MAPA* origem);
-int podeandar(MAPA* m, char personagem, int x, int y);
-int ehparede(MAPA* m, int x, int y);
-int ehpersonagem(MAPA* m, char personagem, int x, int y);
+    typedef struct posicao POSICAO;
 
-int encontramapa(MAPA* m, POSICAO* p, char c);
+    void liberamapa(MAPA* m);
+    void lemapa(MAPA* m);
+    void alocamapa(MAPA* m);
+    int ehvalida(MAPA* m, int x, int y);
+    int ehvazia(MAPA* m, int x, int y);
+    void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
+    void copiamapa(MAPA* destino, MAPA* origem);
+    int podeandar(MAPA* m, char personagem, int x, int y);
+    int ehparede(MAPA* m, int x, int y);
+    int ehpersonagem(MAPA* m, char personagem, int x, int y);
+
+    int encontramapa(MAPA* m, POSICAO* p, char c);
+#endif
